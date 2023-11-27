7e76de4c1a0ad38875cb850d3532df927da40540
Martin Roberto Gaido <119458663+martingaidoo@users.noreply.github.com>
2023-11-27 19:42:42 -0300

    Update Auto.java

--- Auto.java
+++ Auto.java
@@ -12,6 +12,25 @@ public class Auto extends Actor
      * Act - do whatever the Auto wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public void moveForward(int distancia) {
+        int contador = 0;
+        while (contador< distancia){
+            move(1);
+            Greenfoot.delay(50);
+            contador++;
+        }
+        
+    }
+    
+    public void moveBackward(int distancia){
+        int contador = 0;
+        while (contador< distancia){
+            move(-1);
+            Greenfoot.delay(50);
+            contador++;
+        }
+    }
+    
     public void turnLeft(int angulo){
         int i = getRotation();
         while(i/angulo!=1){
