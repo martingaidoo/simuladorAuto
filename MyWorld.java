f38b0db3be29caac2a6aee6aeecc93ab959540a7
MartinCesano <cesano305@gmail.com>
2023-11-27 18:43:18 -0300

    por las dudas

---
+++ MyWorld.java
@@ -0,0 +1,33 @@
+
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+* Write a description of class MyWorld here.
+* 
+* @author (your name) 
+* @version (a version number or a date)
+*/
+public class MyWorld extends World
+{
+
+    /**
+    * Constructor for objects of class MyWorld.
+    * 
+    */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
+
+auto1.moveForward(12);
+auto1.turnRight(90);
+auto1.moveBackward(4);
+auto1.turnLeft(45);
+auto1.moveForward(15);
+auto1.moveBackward(32);
+auto1.moveForward(12);
+auto1.turnRight(90);
+auto1.moveBackward(4);
+auto1.turnLeft(45);
